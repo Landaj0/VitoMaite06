6c281e8446c9789196df9d2a9ec0aa070b9d1a1b
AlexanderLandajo <alexanderlandajo@gmail.com>
2024-12-29 19:21:43 +0100

    Conexion EXITOSA

--- src/main/java/packControl/DatabaseConnection.java
+++ src/main/java/packControl/DatabaseConnection.java
@@ -26,8 +26,13 @@ public class DatabaseConnection {
         if (connection == null) {
             try {
                 // Establece la conexión a la base de datos
+                Class.forName("com.mysql.jdbc.Driver");
                 connection = DriverManager.getConnection(URL, USER, PASSWORD);
                 System.out.println("Conexión exitosa a la base de datos.");
+            } catch (ClassNotFoundException e) {
+                // Manejo de la excepción si el controlador no se encuentra
+                System.err.println("Error: No se encontró el controlador JDBC.");
+                e.printStackTrace();
             } catch (SQLException e) {
                 System.out.println("Error al conectar a la base de datos.");
                 e.printStackTrace();
